44da9c823eb35ca8cccb45b0f6733b2c323f5341
Dennis Keil <dennis.keil10@gmail.com>
2021-12-13 18:51:45 +0100

    update

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -2,12 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {MultiTranslateHttpLoader} from 'ngx-translate-multi-http-loader';
+
+// // AoT requires an exported function for factories
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http, './assets/translate/core/', '.json');
+// }
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new MultiTranslateHttpLoader(http, [
+    {prefix: "./assets/translate/core/", suffix: ".json"},
+    {prefix: "./assets/translate/shared/", suffix: ".json"},
+  ]);
+}
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
